ea98efaf3a2d70ecb0fb1e2f3ed97f6c68b37bc9
Alex Milanov <alex@i4web.biz>
2017-02-16 14:57:48 +0200

    impl. initial slide transitions

--- dist/js/app.js
+++ dist/js/app.js
@@ -13366,21 +13366,29 @@ module.exports = function(sel, data, children, text, elm) {
 // initial
 const initial = {
 	slidesCount: 3,
-	index: 0
+	index: 0,
+	old: 0,
+	transitioning: false
 };
 
 // actions
 const next = () => state => Object.assign({}, state, {
-	index: (state.index < state.slidesCount - 1) ? state.index + 1 : state.index
+	index: (state.index < state.slidesCount - 1) ? state.index + 1 : state.index,
+	old: state.index,
+	transitioning: true
 });
 const prev = () => state => Object.assign({}, state, {
-	index: (state.index > 0) ? state.index - 1 : state.index
+	index: (state.index > 0) ? state.index - 1 : state.index,
+	old: state.index,
+	transitioning: true
 });
+const transitionend = () => state => Object.assign({}, state, {transitioning: false});
 
 module.exports = {
 	initial,
 	next,
-	prev
+	prev,
+	transitionend
 };
 
 },{}],17:[function(require,module,exports){
@@ -13451,33 +13459,48 @@ vdom.patchStream(ui$, '#ui');
 
 // dom
 const {section, button, span, h1, h2, pre, code} = require('iblokz/adapters/vdom');
-// components
-// const counter = require('./counter');
+
+const slides = [
+	// slide 1
+	span([
+		h2('First Steps in'),
+		h1('Functional Reactive JavaScript')
+	]),
+	// slide 2
+	span([
+		h1('creating a Hash'),
+		h2('Converting a collection to a key/value hash:'),
+		pre([code('[type="js"][contenteditable="true"]', `
+const users = [
+	{id: 1, name: 'Bob'},
+	{id: 1, name: 'John'},
+]
+const hash = users.reduce(
+	(h, u) => ((h[u.id] = u.name), h), {}
+);
+		`)])
+	]),
+	// slide 3
+	span([
+		h1('Slide 3'),
+		h2('Some desc here')
+	])
+];
 
 module.exports = ({state, actions}) => section('#ui', [
-	section('.slides[tabindex="0"]', [
-		section({class: {active: state.index === 0}}, [span([
-			h2('First Steps in'),
-			h1('Functional Reactive JavaScript')
-		])]),
-		section({class: {active: state.index === 1}}, [span([
-			h1('creating a Hash'),
-			h2('Converting a collection to a key/value hash:'),
-			pre([code('[type="js"][contenteditable="true"]', `
-	const users = [
-		{id: 1, name: 'Bob'},
-		{id: 1, name: 'John'},
-	]
-	const hash = users.reduce(
-		(h, u) => ((h[u.id] = u.name), h), {}
-	);
-			`)])
-		])]),
-		section({class: {active: state.index === 2}}, [span([
-			h1('Slide 3'),
-			h2('Some desc here')
-		])])
-	])
+	section('.slides[tabindex="0"]', slides.map((slide, i) =>
+		section({
+			class: {
+				active: state.index === i || (state.old === i && state.transitioning === true),
+				onTop: state.transitioning === true && state.index === i,
+				moveFromRight: state.transitioning === true && state.index === i && (state.index - state.old) === 1,
+				moveToLeft: state.transitioning === true && state.old === i && (state.index - state.old) === 1,
+				moveFromLeft: state.transitioning === true && state.index === i && (state.index - state.old) === -1,
+				moveToRight: state.transitioning === true && state.old === i && (state.index - state.old) === -1
+			},
+			on: (state.index === i && state.transitioning) ? {animationend: () => actions.transitionend()} : {}
+		}, [slide])
+	))
 ]);
 
 },{"iblokz/adapters/vdom":1}],19:[function(require,module,exports){
--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -3,19 +3,27 @@
 // initial
 const initial = {
 	slidesCount: 3,
-	index: 0
+	index: 0,
+	old: 0,
+	transitioning: false
 };
 
 // actions
 const next = () => state => Object.assign({}, state, {
-	index: (state.index < state.slidesCount - 1) ? state.index + 1 : state.index
+	index: (state.index < state.slidesCount - 1) ? state.index + 1 : state.index,
+	old: state.index,
+	transitioning: true
 });
 const prev = () => state => Object.assign({}, state, {
-	index: (state.index > 0) ? state.index - 1 : state.index
+	index: (state.index > 0) ? state.index - 1 : state.index,
+	old: state.index,
+	transitioning: true
 });
+const transitionend = () => state => Object.assign({}, state, {transitioning: false});
 
 module.exports = {
 	initial,
 	next,
-	prev
+	prev,
+	transitionend
 };
